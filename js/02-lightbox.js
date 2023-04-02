05c119052e34f401a8000dfb9440a82dd0d714a6
Viktar Baloshka <vboloshko@gmail.com>
2023-04-02 16:33:21 +0200

    finish

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+                <a class="gallery__link" href = "${original}">
+                    <img class=gallery__image
+                        src="${preview}"
+                        alt="${description}">
+                </a>
+            </li>`;
+    })
+    .join('');
+}
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
